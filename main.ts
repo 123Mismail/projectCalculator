4f51fa09e782e138ae9337e57c1afd39ef0a9994
123Mismail <musafirbaltistani786@gmail.com>
2024-03-21 13:25:05 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([
+  { message: "enter your first number", type: "number", name: "firstNumber" },
+  { message: "enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "select your desire operator",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+if (ans.operator === "addition") {
+  console.log(ans.firstNumber + ans.secondNumber);
+} else if (ans.operator === "subtraction") {
+  console.log(ans.firstNumber - ans.secondNumber);
+} else if (ans.operator === "multiplication") {
+  console.log(ans.firstNumber * ans.secondNumber);
+} else if (ans.operator === "division") {
+  console.log(ans.firstNumber / ans.secondNumber);
+} else {
+  console.log("please chose a valid operator");
+}
